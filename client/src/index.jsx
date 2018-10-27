a42635b217a75c7b4f4e499891698826a28854a3
pat-doyle <pdoyle757@gmail.com>
2018-10-26 23:37:52 -0400

    initiated setup for new components to run tests on

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
 class App extends React.Component{
   constructor(props){
     super(props);
+  }
+
+  componentDidMount(){
+    $.get('/')
   }
 
   render() {
--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
-const Express = require('express');
-const app = Express();
-const BodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 const port = 8080;
 
-app.use(BodyParser.json());
+const PicStream = require('./db/photoStream.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.options('/*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.set('Content-Type', 'application/json');
+  res.sendStatus(200);
+});
+
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.get('/', (req, res) => {
   res.send('Placeholder text');
